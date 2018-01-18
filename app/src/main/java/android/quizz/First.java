74536d073d61f495213b16a0839226be6b7693a8
Rupesh Kumar Joshi <rupeshjoshi62@gmail.com>
2018-01-18 16:23:30 +0545

    Framework For Quizz!

--- app/src/main/java/android/quizz/First.java
+++ app/src/main/java/android/quizz/First.java
@@ -1,13 +1,59 @@
 package android.quizz;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class First extends AppCompatActivity {
+    Button b1,b2,b3,b4;
+    @Override
+    public void onBackPressed(){
+        Intent i = new Intent(getApplicationContext(),Main.class);
+        startActivity(i);
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_first);
+        final int sc=10;
+        b1=(Button)findViewById(R.id.b1);
+        b2=(Button)findViewById(R.id.b2);
+        b3=(Button)findViewById(R.id.b3);
+        b4=(Button)findViewById(R.id.b4);
+        b1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent i=new Intent(getApplicationContext(),Loss.class);
+                i.putExtra("Score",sc);
+                startActivity(i);
+            }
+        });
+        b2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent i=new Intent(getApplicationContext(),Loss.class);
+                i.putExtra("Score",sc);
+                startActivity(i);
+            }
+        });
+        b3.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent i=new Intent(getApplicationContext(),Loss.class);
+                i.putExtra("Score",sc);
+                startActivity(i);
+            }
+        });
+        b4.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent i=new Intent(getApplicationContext(),Second.class);
+                startActivity(i);
+            }
+        });
+
     }
 }
